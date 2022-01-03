5635fb184b76acf140b73e8d63f258dbbce01445
Madi <mkuanyshbek97@mail.ru>
2022-01-03 21:30:51 +0600

    click listener

--- app/src/main/java/com/example/a2_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/a2_kotlin/MainActivity.kt
@@ -2,14 +2,23 @@ package com.example.a2_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val value2 = savedInstanceState?.getByte("byteKey")
+        val button = findViewById<Button>(R.id.test_button)
 
-        val value = savedInstanceState!!["valueKey"]
+        button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Tested", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
--- app/src/main/java/com/example/a2_kotlin/Test.kt
+++ app/src/main/java/com/example/a2_kotlin/Test.kt
@@ -11,6 +11,16 @@ class Test {
         }
 
         val intValue = 10
+        var value = 20
+        val byteValue = intValue.toByte()
+
+
+        val arrayList = ArrayList<String>()
+        val arrayList2 = emptyList<String>()
+        val arrayList3 = arrayListOf(0, 1, 2, 3)
+        val mutableArrayList = mutableListOf<String>()
+        mutableArrayList[0] = "hello"
+        val valueList = arrayList[0]
     }
     fun doSomething() {
 
