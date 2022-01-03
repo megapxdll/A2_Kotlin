86161bbd9869d361aae61bcb3f5b9ce7ad4c39ff
Madi <mkuanyshbek97@mail.ru>
2022-01-03 20:50:18 +0600

    test class

--- app/src/main/java/com/example/a2_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/a2_kotlin/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val value2 = savedInstanceState?.getByte("byteKey")
+
+        val value = savedInstanceState!!["valueKey"]
     }
 }
---
+++ app/src/main/java/com/example/a2_kotlin/Test.kt
@@ -0,0 +1,22 @@
+package com.example.a2_kotlin
+
+class Test {
+
+    fun start() {
+        var result = doSomething()
+        if (result is Unit) {
+
+        } else {
+
+        }
+
+        val intValue = 10
+    }
+    fun doSomething() {
+
+    }
+
+    fun doSomething(a: Int) : String {
+        return ""
+    }
+}
